6584764668a330afd0879ffc27bcddd194773511
abhinavbahuguna2002 <speakabhinav1@gmail.com>
2023-05-31 17:48:58 +0530

    Add files via upload

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,3 +1,25 @@
+'''
+The given code defines a class `DataIngestionConfig` using the `@dataclass` decorator. It has three attributes `train_data_path`, `test_data_path`, and `raw_data_path`, which are initialized with default values using the `os.path.join()` function. 
+These default values are relative file paths pointing to CSV files located in the "artifacts" directory.
+
+Next, the code defines a class `DataIngestion`, which has an `__init__` method that creates an instance of `DataIngestionConfig` and assigns it to the `ingestion_config` attribute.
+
+The class `DataIngestion` also has a method called `initiate_data_ingestion`, which performs the following steps:
+
+1. Reads a CSV file named "stud.csv" located in the "notebook\data" directory and stores it as a Pandas DataFrame.
+2. Creates the directory structure for the `train_data_path` using `os.makedirs()` if it doesn't already exist.
+3. Saves the DataFrame as a CSV file at `raw_data_path` location.
+4. Splits the DataFrame into a training set and a test set using the `train_test_split` function from scikit-learn, with a test size of 0.2 and a random state of 42.
+5. Saves the training set as a CSV file at `train_data_path` location and the test set as a CSV file at `test_data_path` location.
+6. Logs the progress and completion of the data ingestion process.
+7. Finally, it returns a tuple containing the paths of the generated training and test data files.
+
+If any exception occurs during the data ingestion process, it raises a custom exception `CustomException`, passing the original exception `e` and the `sys` module.
+
+The `if __name__ == "__main__"` block creates an instance of the `DataIngestion` class and calls the `initiate_data_ingestion` method, triggering the execution of the data ingestion process when the script is run directly.
+
+'''
+
 import os 
 import sys 
 import pandas as pd
@@ -8,7 +30,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
-@dataclass
+@dataclass #decorator
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts', 'train.csv')
     test_data_path: str=os.path.join('artifacts', 'test.csv')
@@ -34,6 +56,7 @@ class DataIngestion:
             train_set, test_set= train_test_split (df, test_size=0.2, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+
             #saving test file
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
@@ -47,6 +70,3 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
 
-if __name__ =="__main__":
-    obj=DataIngestion()
-    obj.initiate_data_ingestion()
